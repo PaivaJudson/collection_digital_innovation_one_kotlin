7954da69bcd8179d6a48f48c6c9c16b0f018dfc3
Judson Paiva <judson.paiva@hotmail.com>
2021-09-01 11:34:15 +0100

    Iterar o IntArray

--- src/main/kotlin/org/example/TesteIntArray.kt
+++ src/main/kotlin/org/example/TesteIntArray.kt
@@ -1,5 +1,17 @@
 package org.example
 
 fun main() {
-    
+
+    val valores = IntArray(5)
+
+    valores[0] = 1
+    valores[1] = 4
+    valores[2] = 3
+    valores[3] = 7
+    valores[4] = 6
+
+    for ((key, value) in valores.withIndex() ){
+        println("i = $key valor = $value")
+    }
+
 }
