86ac962c4a6fb24096bbd8421e86acc2f9074ea6
Judson Paiva <judson.paiva@hotmail.com>
2021-09-01 14:10:29 +0100

    Trabalhando com find, any e count

--- src/main/kotlin/org/example/TesteOperacoes.kt
+++ src/main/kotlin/org/example/TesteOperacoes.kt
@@ -14,10 +14,23 @@ fun main() {
     println("Salário médio ${salarios.average()}")
 
 
-    val salariosMaiorQue2500 = salarios.filter { it>2000.0 }
+
     println("------------------------------------------------------------------")
+    val salariosMaiorQue2500 = salarios.filter { it>2000.0 }
     for (salario in salariosMaiorQue2500){
         println(salario)
     }
-    
+
+    println("------------------------------------------------------------------")
+    //println(salarios.count())
+    println(salarios.count { it in 2000.0 .. 5000.0 })
+
+
+    println("------------------------------------------------------------------")
+    println(salarios.find { it == 2250.0 })
+
+    println("------------------------------------------------------------------")
+    println(salarios.any { it == 1000.0 })
+
+
 }
