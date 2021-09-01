a6889f309d0f1e12b0cbe13025bba27ff8e817ea
Judson Paiva <judson.paiva@hotmail.com>
2021-09-01 13:31:49 +0100

    Trabalhando forEachIndexed

--- src/main/kotlin/org/example/TesteDoubleArray.kt
+++ src/main/kotlin/org/example/TesteDoubleArray.kt
@@ -1,2 +1,14 @@
 package org.example
 
+fun main() {
+
+    val salarios = DoubleArray(4){(it+1)*1000.0}
+
+    for ((i, v) in salarios.withIndex()){
+        println("$i tem o salário = $v")
+    }
+
+    println("-----------------------------------------------------")
+    salarios.forEachIndexed { index, d -> salarios[index] = salarios[index] + d * 0.2 }
+    salarios.forEach { println(it) }
+}
