ade47190ed66842c1afca7399e1f560f6b541cbc
Judson Paiva <judson.paiva@hotmail.com>
2021-09-01 14:59:33 +0100

    organizando listas com groupBy

--- src/main/kotlin/org/example/TesteList.kt
+++ src/main/kotlin/org/example/TesteList.kt
@@ -1,6 +1,6 @@
 package org.example
 
-data class Funcionario(val nome: String, val salario: Double){
+data class Funcionario(val nome: String, val salario: Double, val tipoContratacao:String){
     override fun toString(): String {
         return "Nome:   $nome | Salário = $salario"
     }
@@ -9,9 +9,9 @@ data class Funcionario(val nome: String, val salario: Double){
 
 fun main() {
 
-    val f1 = Funcionario("Leonor José", 4000.0)
-    val f2 = Funcionario("Judson Paiva", 4000.0)
-    val f3 = Funcionario("Jéssica Paiva", 2500.0)
+    val f1 = Funcionario("Leonor José", 4000.0, "CLT")
+    val f2 = Funcionario("Judson Paiva", 4000.0, "PJ")
+    val f3 = Funcionario("Jéssica Paiva", 2500.0, "CLT")
 
     val funcionarios = listOf(f1, f2, f3)
     funcionarios.forEach { println(it) }
@@ -24,5 +24,11 @@ fun main() {
 
     println("------------------------------------------------------------")
     funcionarios.sortedBy { it.salario }.forEach { println(it) }
+
+
+    println("------------------------------------------------------------")
+    funcionarios.groupBy { it.tipoContratacao }.forEach { println(it) }
+
+    println("------------------------------------------------------------")
 
 }
