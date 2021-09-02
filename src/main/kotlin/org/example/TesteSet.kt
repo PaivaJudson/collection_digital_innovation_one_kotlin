0ad67535008863434410a67f70c128141831f17b
Judson Paiva <judson.paiva@hotmail.com>
2021-09-02 23:53:31 +0100

    Trabalhancom a estrutura Set, criação do ficheiro TesteSet

---
+++ src/main/kotlin/org/example/TesteSet.kt
@@ -0,0 +1,13 @@
+package org.example
+
+fun main() {
+
+    val f1 = Funcionario("Leonor José", 4000.0, "CLT")
+    val f2 = Funcionario("Judson Paiva", 4000.0, "PJ")
+    val f3 = Funcionario("Jéssica Paiva", 2500.0, "CLT")
+
+
+
+
+
+}
