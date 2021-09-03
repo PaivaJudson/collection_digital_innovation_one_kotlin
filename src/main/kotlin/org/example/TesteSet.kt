11fe08376d159e52185896cccb3f1ef17abafabd
Judson Paiva <judson.paiva@hotmail.com>
2021-09-03 11:15:35 +0100

    Trabalhancom com conjuntos usando setOf, union, subtract, intersect and subtract

--- src/main/kotlin/org/example/TesteSet.kt
+++ src/main/kotlin/org/example/TesteSet.kt
@@ -5,16 +5,26 @@ fun main() {
     val f1 = Funcionario("Leonor José", 4000.0, "CLT")
     val f2 = Funcionario("Judson Paiva", 4000.0, "PJ")
     val f3 = Funcionario("Jéssica Paiva", 2500.0, "CLT")
+    val f4 = Funcionario("Anderson Paiva", 3000.0, "CLT")
 
-
-    val lista1 = setOf(f1, f2)
-    val lista2 = setOf(f3)
+    val lista1 = setOf(f1, f3)
+    val lista2 = setOf(f2)
 
     val resultadoUnion = lista1.union(lista2)
-
     resultadoUnion.forEach { println(it) }
 
     println("-------------------------------------------------")
-    val resultadoSub = resultadoUnion.subtract(lista2)
+    val lista_2 = lista2.union(setOf(f4))
+    lista_2.forEach { println(it) }
+
+    println("-------------------------------------------------")
+    val resultadoSub = resultadoUnion.subtract(lista_2)
+    resultadoSub.forEach { println(it) }
+
+    println("-------------------------------------------------")
+    val resultInterSec = resultadoUnion.intersect(lista_2)
+
+    resultInterSec.forEach { println(it) }
+
 
 }
